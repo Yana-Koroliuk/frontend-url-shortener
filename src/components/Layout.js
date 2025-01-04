7ea7e709a3397b86b1d7d9b0eea900291869075b
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2025-01-04 07:13:25 +0200

    Update style of header and layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,11 +2,11 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 const Layout = ({ children, isAuthenticated, onLogout }) => (
-    <>
+    <div className="flex flex-col min-h-screen">
         <Header isAuthenticated={isAuthenticated} onLogout={onLogout} />
-        <main className="p-4">{children}</main>
+        <main className="flex-grow container mx-auto p-6">{children}</main>
         <Footer />
-    </>
+    </div>
 );
 
 export default Layout;
