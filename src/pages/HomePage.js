b1263cbc7dca9aa84de90e9ccf8ef0f622be8395
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2025-01-04 07:13:43 +0200

    Update style of home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import Layout from "../components/Layout";
-import {logout} from "../api/auth";
+import { logout } from "../api/auth";
 
 const HomePage = () => {
     const navigate = useNavigate();
@@ -15,12 +15,19 @@ const HomePage = () => {
 
     return (
         <Layout isAuthenticated={isAuthenticated} onLogout={handleLogout}>
-            <div className="text-center p-4">
+            <div className="flex flex-col items-center justify-center text-center">
                 {isAuthenticated ? (
-                    <h1 className="text-2xl font-bold">Hello, {fullName || username}</h1>
+                    <h1 className="text-4xl font-bold text-blue-700 mb-4">
+                        Hello, {fullName || username}!
+                    </h1>
                 ) : (
-                    <h1 className="text-2xl font-bold">You aren't authenticated</h1>
+                    <h1 className="text-4xl font-bold text-blue-700 mb-4">
+                        You aren't authenticated.
+                    </h1>
                 )}
+                <p className="text-lg text-gray-600">
+                    Welcome to Link Shortener! Easily create, manage, and share shortened URLs.
+                </p>
             </div>
         </Layout>
     );
